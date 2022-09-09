390c383c6758892c4553e67e09152b447e3ebe50
NataTassi <natanieltassi@gmail.com>
2022-09-09 14:57:25 -0300

    some updates

--- lib/convert_audios_in_folder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in *.wav; do
-    [ -f "$i" ] || break
-
-    filename="${i%.*}"
-    output="${filename}.mp3"
-    ffmpeg -y -i $i -ab 320k -ar 48k $output
-    mv $i old/$i
-done
