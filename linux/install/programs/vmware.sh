0affc8145eed384564fbce9a7d9f65098888690e
Nataniel Tassi <natanieltassi@gmail.com>
2022-12-27 18:14:42 -0300

    Add authentication, base64, and some updates

--- linux/install/programs/vmware.sh
+++ linux/install/programs/vmware.sh
@@ -1,6 +1,13 @@
-vmware_installer="vmware_player_16.2.4.bundle"
-wget -q --show-progress https://download3.vmware.com/software/WKST-PLAYER-1624/VMware-Player-Full-16.2.4-20089737.x86_64.bundle -O $vmware_installer
-chmod +x $vmware_installer
-sudo ./$vmware_installer
-sudo apt update && sudo apt install gcc make
+version="17.0.0"
+#vmware_installer="vmware_player_${version}.bundle"
+#wget -q --show-progress https://download3.vmware.com/software/WKST-PLAYER-1700/VMware-Player-Full-17.0.0-20800274.x86_64.bundle -O $vmware_installer
+#chmod +x $vmware_installer
+#sudo ./$vmware_installer
+#sudo apt update && sudo apt install gcc make
+wget -q --show-progress https://github.com/mkubecek/vmware-host-modules/archive/workstation-${version}.tar.gz -O vmware-host-modules-${version}.tar.gz
+tar -xf vmware-host-modules-${version}.tar.gz
+cd vmware-host-modules-workstation-${version}
+tar -cf vmmon.tar vmmon-only
+tar -cf vmnet.tar vmnet-only
+sudo cp -v vmmon.tar vmnet.tar /usr/lib/vmware/modules/source/
 sudo vmware-modconfig --console --install-all
