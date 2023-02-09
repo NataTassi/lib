cb594afb14f2a9e52366427abdbc646c181aff79
Nataniel Tassi <natanieltassi@gmail.com>
2023-02-09 15:15:10 -0300

    Add VMware install script

--- linux/install/programs/vmware.sh
+++ linux/install/programs/vmware.sh
@@ -1,13 +1,8 @@
-version="17.0.0"
-#vmware_installer="vmware_player_${version}.bundle"
-#wget -q --show-progress https://download3.vmware.com/software/WKST-PLAYER-1700/VMware-Player-Full-17.0.0-20800274.x86_64.bundle -O $vmware_installer
-#chmod +x $vmware_installer
-#sudo ./$vmware_installer
-#sudo apt update && sudo apt install gcc make
-wget -q --show-progress https://github.com/mkubecek/vmware-host-modules/archive/workstation-${version}.tar.gz -O vmware-host-modules-${version}.tar.gz
-tar -xf vmware-host-modules-${version}.tar.gz
-cd vmware-host-modules-workstation-${version}
-tar -cf vmmon.tar vmmon-only
-tar -cf vmnet.tar vmnet-only
-sudo cp -v vmmon.tar vmnet.tar /usr/lib/vmware/modules/source/
-sudo vmware-modconfig --console --install-all
+wget -q --show-progress "https://download3.vmware.com/software/WKST-PLAYER-1701/VMware-Player-Full-17.0.1-21139696.x86_64.bundle"
+installer="VMware-Player-Full-17.0.1-21139696.x86_64.bundle"
+sudo apt install gcc build-essential -y # install required packages
+chown +x $installer
+sudo ./$installer
+sudo vmware-modconfig --console --install-all # install kernel modules
+echo "You may need to disable UEFI Secure Boot for VMware to work"
+vmplayer
