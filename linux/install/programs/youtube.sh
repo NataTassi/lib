720b076684addd35f999a89185bcb5af6b6e9eb7
Nataniel Tassi <natanieltassi@gmail.com>
2023-06-29 21:36:25 -0300

    Add dual boot install, youtube install and firefox shortcuts

---
+++ linux/install/programs/youtube.sh
@@ -0,0 +1,4 @@
+sudo wget -q --show-progress https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt
+sudo chmod +x /usr/local/bin/yt
+cd /usr/bin
+sudo ln -fs /usr/bin/python3 python
